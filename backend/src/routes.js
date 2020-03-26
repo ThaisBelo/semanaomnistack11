0e4acd07032671abd32358fe4d174f02e0a7fa82
Thais Ribeiro Belo <thais.officer@gmail.com>
2020-03-26 17:28:35 -0300

    Backend da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,53 @@
+const express= require ('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
+const routes = express.Router();
+
+
+/**
+ * Rota/ Recurso
+ */
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**Tipos de parâmetros
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, Paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+  */
+
+  /**
+   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+   * NoSQL: MongoDB, CouchDB,
+   */
+
+   /**
+    * Driver: SELECT* FROM users
+    * Query Builder: table('users'). select ('*'), where()
+    */
+
+   routes.post('/session', SessionController.create);
+
+   routes.get('/ongs', OngController.index);
+   routes.post('/ongs', OngController.create);
+
+   routes.get('/incidents', IncidentController.index);
+   routes.post('/incidents', IncidentController.create);
+   routes.delete('/incidents/:id', IncidentController.delete);
+   
+   routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
+
